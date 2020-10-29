5cef403804fefa7c2f7db521199daa026af26fb3
VinsonCernuto <68867988+VinsonCernuto@users.noreply.github.com>
2020-10-29 13:11:38 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -4,10 +4,10 @@ require('dotenv').config();
 module.exports = {
   "development": {
 
-    "username":"fy7pbbrkyw1tptn9",
-    "password": "d5vaee9zjw5kvqp0",
-    "database": "ut8hr2ydzksiu277",
-    "host": "vrk7xcrab1wsx4r1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    "username": "tipj50h0xl0rlbup",
+    "password": "qoln8ovnfx3m50kj",
+    "database": "plants",
+    "host": "sh4ob67ph9l80v61.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
 
      "dialect": "mysql"
   },
